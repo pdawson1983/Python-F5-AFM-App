4e23a9edb4fb3b56b4b4045bdb7c1de2dfd6e603
pdawson1983 <pdawson1983@gmail.com>
2016-01-20 15:30:40 -0500

    Add additional features
    Added favicon support, and additional route to the home page.  Also pointing requests pointing home to the home template.

--- F5AFMApp.py
+++ F5AFMApp.py
@@ -1,14 +1,17 @@
-from flask import Flask, url_for
+import os
+from flask import Flask, url_for, send_from_directory, get_flashed_messages, session, request, render_template
 
 app = Flask(__name__)
-
+#Added Favicon Support
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
+#Point default Traffic to home template
 @app.route('/')
+@app.route('/home')
 def home():
-    text = '<a href=%s>link</a>' % url_for('home')
-    return text
+    return render_template('home.html')
 
-
-
+@app.route
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
-
