30019eab0e8769f630c305b16736ff9e5039b880
pdawson1983 <pdawson1983@gmail.com>
2016-01-21 15:44:02 -0500

    Update F5AFMApp.py
    Added additional routing

--- F5AFMApp.py
+++ F5AFMApp.py
@@ -1,6 +1,16 @@
 import os
 from flask import Flask, url_for, send_from_directory, get_flashed_messages, session, request, render_template
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from configure_database import Base, F5Device, AFMStat, StatValue
 
+engine = create_engine('sqlite:///F5AFM_App.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+#spawn Flask application
 app = Flask(__name__)
 #Added Favicon Support
 @app.route('/favicon.ico')
@@ -12,13 +22,31 @@ def favicon():
 def home():
     return render_template('home.html')
 
-@app.route('/device/')
+@app.route('/devices/')
 def deviceList():
-    pass
+    devices = session.query(F5Device).all()
+    print devices
+    return render_template('devicelist.html', devices=devices)
+
+@app.route('/device/<int:device_id>')
+def device(device_id):
+    return "%s" % device_id
+
+@app.route('/device/<int:device_id>/stats/<int:stats_id>')
+def statPage(device_id, stats_id):
+    text = ''
+    text += "%s, %s" %(device_id, stats_id)
+    return text
+
+@app.route('/device/<int:device_id>/stats/new')
+def newStat():
+	return "This page will allow you to add stats. :)"
 
 @app.route('/device/new')
 def newDevice():
-    pass
+	return "This page will allow you to add devices. :)"
+
+ 
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
