dee13f796bfa74d51d625977a439665ed2f15d84
Paul Dawson <pdawson1983@gmail.com>
2016-02-02 03:46:30 +0000

    Completed New Device Form

--- static/directives.js
+++ static/directives.js
@@ -12,8 +12,26 @@ var f5appDirectives = {}
 //directives
 
 
-f5appDirectives.ngValidate = function() {
-	return {}
+f5appDirectives.ipValidate = function() {
+	return {
+		//argument only
+		restrict: 'A',
+
+		require: 'ngModel',
+
+		link: function (scope, element, attr, ctrl){
+
+			function ipValidator(inputValue){
+				if (/\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/.test(inputValue)){
+					ctrl.$setValidity('validIpAddress', true);
+				}else{
+					ctrl.$setValidity('validIpAddress', false);
+				}
+				return inputValue;
+			}
+			ctrl.$parsers.push(ipValidator);
+		}
+	};
 };
 
 
